00abecb568a7bfed8a50eea1e3a24f3db8184335
dlepke <deannalepke@gmail.com>
2023-03-11 19:02:07 -0800

    added msg/response flow (placeholder)

--- public/js/index.js
+++ public/js/index.js
@@ -13,17 +13,28 @@ getData();
 
 function addUserMessage() {
   const input = document.getElementById("chatEntry");
-  const msg = input.value;
-  console.log(msg);
-  // add usr msg
-  const messageList = document.getElementById("messageList");
-  const newMessage = document.createElement("p");
-  const textNode = document.createTextNode(msg);
-  newMessage.append(textNode);
-  messageList.append(newMessage);
-  getBotReply();
+  const msg = input.value.trim();
+  if (msg !== "") {
+    console.log(msg);
+    input.value = "";
+    // add usr msg
+    const messageList = document.getElementById("messageList");
+    const newMessage = document.createElement("p");
+    newMessage.classList.add("userMsg");
+    const textNode = document.createTextNode(msg);
+    newMessage.append(textNode);
+    messageList.append(newMessage);
+    getBotReply();
+  }
 }
 
 function getBotReply() {
   console.log("I'm from bot!");
+  const msg = "hello, banana";
+  const messageList = document.getElementById("messageList");
+  const newMessage = document.createElement("p");
+  newMessage.classList.add("botMsg");
+  const textNode = document.createTextNode(msg);
+  newMessage.append(textNode);
+  messageList.append(newMessage);
 }
